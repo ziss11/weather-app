eed494e03f41d55da06cd094faa48a94f7625b00
Abdul Azis <abdul.azizz1108@gmail.com>
2024-02-10 11:44:15 +0700

    feat(home): add observer when the time is day or night and change the app color

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -2,20 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weather_app/common/constants.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with WidgetsBindingObserver {
   final isNight = false.obs;
   final isRefresh = false.obs;
   final tempUnit = Constants.celcius.obs;
 
   @override
-  void onReady() {
-    super.onReady();
+  onInit() {
+    super.onInit();
+
     _checkIsNight();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  onClose() {
+    super.onClose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    debugPrint(state.toString());
+
+    if (state == AppLifecycleState.resumed) {
+      _checkIsNight();
+    }
   }
 
   void _checkIsNight() {
     final hour = DateTime.now().hour;
-    debugPrint(hour.toString());
     isNight.value = hour >= 18 || hour <= 6;
   }
 
